9c8d62343c618a8bacf8849588786dcee2672683
JavierCastillo2 <66886448+JavierCastillo2@users.noreply.github.com>
2021-08-08 12:27:24 -0500

    again index
    index

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('layouts.enlace', function () {
     return view('enlace');
 });
-Route::get('/', function () {
+Route::get('index', function () {
     return view('index');
 });
 Route::get('about', function () {
